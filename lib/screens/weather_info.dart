5790a80c7c9af7e652d97c3d83d288709bbb414c
fatyga <fatydzak12@gmail.com>
2022-09-28 13:17:37 +0200

    Add svg icons and update WeatherInfo UI

--- lib/screens/weather_info.dart
+++ lib/screens/weather_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WeatherInfo extends StatefulWidget {
   @override
@@ -14,20 +15,43 @@ class _WeatherInfoState extends State<WeatherInfo> {
     return Scaffold(
       appBar: AppBar(title: const Text('Weather App')),
       body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+        padding: EdgeInsets.fromLTRB(15, 30, 15, 15),
         child: Row(children: <Widget>[
           Expanded(
             child: Column(children: [
-              Text('${data['data']['locationName']}'),
-              Text(data['data']['time']),
+              Text('${data['data']['locationName']}'.toUpperCase(),
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
+              Text(data['data']['time'].toUpperCase()),
+              const SizedBox(height: 50),
               Image.network(data['data']['icon']),
-              Text(data['data']['description']),
-              Text('Temperature: ${data['data']['temp']}'),
-              Text('Min. Temperature: ${data['data']['minTemp']}'),
-              Text('Feels like Temperature: ${data['data']['feelsLike']}'),
-              Text('Max. Temperature: ${data['data']['minTemp']}'),
-              Text('Humidity: ${data['data']['humidity']}'),
-              Text('Pressure: ${data['data']['pressure']}'),
+              const SizedBox(height: 50),
+              Text(data['data']['description'].toUpperCase(),
+                  style: TextStyle(color: Colors.grey[600])),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: [
+                      Text('min',
+                          style:
+                              TextStyle(fontSize: 10, color: Colors.grey[600])),
+                      Text(data['data']['minTemp']),
+                    ],
+                  ),
+                  Text('${data['data']['temp']}',
+                      style: const TextStyle(fontSize: 40)),
+                  Column(
+                    children: [
+                      Text('max',
+                          style:
+                              TextStyle(fontSize: 10, color: Colors.grey[600])),
+                      Text(data['data']['minTemp']),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
               ElevatedButton(
                   onPressed: () {
                     Navigator.pushReplacementNamed(context, '/');
