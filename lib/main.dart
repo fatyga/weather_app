d54249966fabc67e58636f647521b3236f551b7f
fatyga <fatydzak12@gmail.com>
2022-10-09 11:55:20 +0200

    Add weather_app file

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/screens/start_screen.dart';
+import 'package:weather_app/weather_app.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      debugShowCheckedModeBanner: false,
-      home: StartScreen(),
-    );
-  }
 }
